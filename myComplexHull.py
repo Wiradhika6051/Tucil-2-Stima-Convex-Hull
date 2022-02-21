360e0c10ad97e7b147690868e30892f21abd2cd6
Fawwaz Anugrah W D <69834655+Wiradhika6051@users.noreply.github.com>
2022-02-21 23:24:58 +0700

    Setup buat tucil + bikin struktur kerangka kelasnya

---
+++ myComplexHull.py
@@ -0,0 +1,12 @@
+class MyComplexHull:
+    def __init__(self,coordinates):
+        self.coordinates = coordinates
+        self.simplices = self.getConVexHull(coordinates)
+    def sort():
+        #pake quicksort
+        pass 
+    def getConvexHUll(coordinates):
+        #dapetin list convexhull
+        pass
+    def checkPosition(point_start,point_finish,point_test):
+        pass
